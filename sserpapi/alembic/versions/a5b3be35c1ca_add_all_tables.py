237b62b9e34d878cf35700b7ebf3a2cf22bc154d
Saiful Islam <saiful.kbtel@gmail.com>
2023-10-02 13:06:49 +0600

    Fix: reinitialize alembic and execute first revision

--- sserpapi/alembic/versions/a5b3be35c1ca_add_all_tables.py
+++ sserpapi/alembic/versions/a5b3be35c1ca_add_all_tables.py
@@ -1,19 +1,21 @@
 """Add all tables
 
-Revision ID: cb73214a3d94
+Revision ID: a5b3be35c1ca
 Revises: 
-Create Date: 2023-09-26 15:23:06.257736
+Create Date: 2023-10-02 12:59:46.209011
 
 """
+from typing import Sequence, Union
+
 from alembic import op
 import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'cb73214a3d94'
-down_revision = None
-branch_labels = None
-depends_on = None
+revision: str = 'a5b3be35c1ca'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
@@ -25,6 +27,15 @@ def upgrade() -> None:
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_clients_name'), 'clients', ['name'], unique=True)
+    op.create_table('users',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('user_name', sa.String(), nullable=True),
+    sa.Column('email', sa.String(), nullable=True),
+    sa.Column('full_name', sa.String(), nullable=True),
+    sa.Column('disabled', sa.Boolean(), nullable=True),
+    sa.Column('password', sa.String(), nullable=True),
+    sa.PrimaryKeyConstraint('id')
+    )
     op.create_table('vendors',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(), nullable=True),
@@ -69,6 +80,7 @@ def downgrade() -> None:
     op.drop_index(op.f('ix_contacts_name'), table_name='contacts')
     op.drop_table('contacts')
     op.drop_table('vendors')
+    op.drop_table('users')
     op.drop_index(op.f('ix_clients_name'), table_name='clients')
     op.drop_table('clients')
     # ### end Alembic commands ###
