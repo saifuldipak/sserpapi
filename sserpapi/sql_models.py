968e56db52d75fcb5cfd20b45e0f3471e1c56ef6
Saiful Islam <saiful.kbtel@gmail.com>
2023-10-03 14:40:20 +0600

    Fix: correct module imports in different modules

--- sserpapi/sql_models.py
+++ sserpapi/sql_models.py
@@ -1,4 +1,4 @@
-from sserpapi.db_connection import Base
+from db_connection import Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 
