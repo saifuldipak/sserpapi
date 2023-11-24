168631ca3ce7073aaec3586e93fc435fd1ee961d
Saiful Islam <saiful.kbtel@gmail.com>
2023-11-24 12:07:37 +0600

    Feat: replace sqlite with postgresql

--- sserpapi/db_connection.py
+++ sserpapi/db_connection.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./simple_erp.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+#SQLALCHEMY_DATABASE_URL = 'sqlite:///./simple_erp.db'
+SQLALCHEMY_DATABASE_URL = "postgresql://saiful:saiful8146@127.0.0.1/sserp"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 Base = declarative_base()
