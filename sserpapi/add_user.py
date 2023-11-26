b121b372440203744dd23db841181b8612fd8f40
Saiful Islam <saiful.kbtel@gmail.com>
2023-11-26 14:39:49 +0600

    Fix: add 'email' property when creating 'User' class instance

--- sserpapi/add_user.py
+++ sserpapi/add_user.py
@@ -3,10 +3,11 @@ from auth import get_password_hash
 from db_queries import get_user_by_name, add_user
 from pydantic_schemas import User
 
-user_name = 'test2'
-password = 'test123'
+user_name = 'saiful'
+password = 'amisaiful'
 disabled = False
 scope = 'user'
+email = 'saiful@somedoamin.com'
 
 try:
     db = SessionLocal()
@@ -15,7 +16,7 @@ try:
         print(f'User exists: {user_name}')
     else:
         hashed_password = get_password_hash(password)
-        new_user = User(user_name=user_name, password=hashed_password, disabled=disabled, scope=scope)
+        new_user = User(user_name=user_name, password=hashed_password, email=email, disabled=disabled, scope=scope)
         new_user = add_user(db=db, user=new_user)
         print(f'User created: {new_user.user_name}')
 except Exception as e:
