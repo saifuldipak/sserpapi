80595d2e7895dc750d4f90c607d444a8ce3bf91e
Saiful Islam <saiful.kbtel@gmail.com>
2023-11-26 12:35:07 +0600

    Fix: remove 'user_add' function and add logic inside try block

--- sserpapi/add_user.py
+++ sserpapi/add_user.py
@@ -1,22 +1,23 @@
 from db_connection import SessionLocal
 from auth import get_password_hash
 from db_queries import get_user_by_name, add_user
+from pydantic_schemas import User
 
-USER = 'test'
-PASSWORD = 'test123'
-
-def user_add(db, user_name, password):
-        user_exists = get_user_by_name(db=db, user_name=user_name)
-        if user_exists:
-            return f'User exists: {user_name}'
-        else:
-            hashed_password = get_password_hash(password)
-            new_user= add_user(db=db, user_name=user_name, password=hashed_password)
-            return f'User created: {new_user.user_name}'
+user_name = 'test2'
+password = 'test123'
+disabled = False
+scope = 'user'
 
 try:
     db = SessionLocal()
-    print(user_add(db=db, user_name=USER, password=PASSWORD))
+    user_exists = get_user_by_name(db=db, user_name=user_name)
+    if user_exists:
+        print(f'User exists: {user_name}')
+    else:
+        hashed_password = get_password_hash(password)
+        new_user = User(user_name=user_name, password=hashed_password, disabled=disabled, scope=scope)
+        new_user = add_user(db=db, user=new_user)
+        print(f'User created: {new_user.user_name}')
 except Exception as e:
     print(f'An error occured: {e}')
 finally:
