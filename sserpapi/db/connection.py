fe1e9f708e5183bc0e73714f302eb4cc97e2c43b
Saiful Islam <saiful.kbtel@gmail.com>
2024-03-28 15:17:19 +0600

    Fix: read postgres username, password, database from a text file

--- sserpapi/db/connection.py
+++ sserpapi/db/connection.py
@@ -1,9 +1,29 @@
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL = 'sqlite:///./simple_erp.db'
-SQLALCHEMY_DATABASE_URL = "postgresql://saiful:saiful8146@127.0.0.1/sserp"
+logger = logging.getLogger(__name__)
+
+POSTGRES_SECRETS = "./db/postgres_secrets.txt"
+
+# Function to read config file and extract database connection info
+def read_db_config(file_path):
+    config = {}
+    with open(file_path, "r", encoding="utf-8") as file:
+        for line in file:
+            try:
+                key, value = line.strip().split("=")
+                config[key] = value
+            except ValueError:
+                logger.warning('Issue with line: %s', line.strip())
+    return config
+
+# Read database connection info from config file
+db_config = read_db_config(POSTGRES_SECRETS)
+
+# Construct database URL
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_config['POSTGRES_USER']}:{db_config['POSTGRES_PASSWORD']}@{db_config['POSTGRES_HOST']}:{db_config['POSTGRES_PORT']}/{db_config['POSTGRES_DB']}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
