061ffc5509f55b6972955bbca2eda32ccfaf5ba1
Saiful Islam <saiful.kbtel@gmail.com>
2024-04-21 13:09:06 +0600

    Feat: use python-dotenv to store sensitive info

--- sserpapi/db/connection.py
+++ sserpapi/db/connection.py
@@ -1,34 +1,28 @@
 import logging
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sserpapi.logger_config import create_file_handler
 
+# Loading environment variables
+load_dotenv()
+
 logger = logging.getLogger(__name__)
 file_handler = create_file_handler()
 logger.addHandler(file_handler)
 
-POSTGRES_SECRETS = ".env"
-
-# Function to read config file and extract database connection info
-def read_db_config(file_path):
-    config = {}
-    with open(file_path, "r", encoding="utf-8") as file:
-        for line in file:
-            try:
-                key, value = line.strip().split("=")
-                config[key] = value
-            except ValueError:
-                logger.warning('Issue with line: %s', line.strip())
-    return config
-
-# Read database connection info from config file
-db_config = read_db_config(POSTGRES_SECRETS)
+POSTGRES_USER=os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD=os.getenv('POSTGRES_PASSWORD')
+POSTGRES_HOST=os.getenv('POSTGRES_HOST')
+POSTGRES_PORT=os.getenv('POSTGRES_PORT')
+POSTGRES_DB=os.getenv('POSTGRES_DB')
 
 # Construct database URL
-DB_URL = f"""postgresql://{db_config['POSTGRES_USER']}:{db_config['POSTGRES_PASSWORD']}\
-@{db_config['POSTGRES_HOST']}:{db_config['POSTGRES_PORT']}/{db_config['POSTGRES_DB']}"""
-
+DB_URL = f"""postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}\
+@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"""
+print(DB_URL)
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 Base = declarative_base()
