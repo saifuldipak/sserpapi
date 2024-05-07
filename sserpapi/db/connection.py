9ef0ff2962f359a0c62e74ecf0098280a4eecc3e
Saiful Islam <saiful.kbtel@gmail.com>
2024-05-07 15:00:43 +0600

    Fix: change declarative_base import statement

--- sserpapi/db/connection.py
+++ sserpapi/db/connection.py
@@ -2,7 +2,7 @@ import os
 import logging
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 logger = logging.getLogger(__name__)
