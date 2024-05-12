5a1c9ed35c6ffaa2dd14b287baa48cde835da8bf
Saiful Islam <saiful.kbtel@gmail.com>
2024-05-12 12:08:29 +0600

    Fix: move all tests related to client create to test_create_client()

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -16,6 +16,12 @@ new_client = {
     'client_type_id': 1
 }
 
+another_new_client = {
+    'id': 0,
+    'name': 'another_test_client',
+    'client_type_id': 1
+}
+
 updated_client = {
     'name': 'test_client_updated',
     'client_type_id': 2
@@ -60,13 +66,29 @@ def test_create_client(auth_header):
     assert create_response.status_code == 200
     assert create_response.json()['name'] == new_client['name']
     assert create_response.json()['client_type_id'] == new_client['client_type_id']
+    client_id = create_response.json()['id']
     
     get_response = client.get(f'/clients?client_name={create_response.json()["name"]}', headers=auth_header)
     assert get_response.status_code == 200
     assert get_response.json()[0]['name'] == new_client['name']
     assert get_response.json()[0]['client_type_id'] == new_client['client_type_id']
 
-    delete_response = client.delete(f'/client/{create_response.json()["id"]}', headers=auth_header)
+    create_response = client.post('/client', json=new_client, headers=auth_header)
+    assert create_response.status_code == 400
+    assert create_response.json()['detail'] == 'Client exists'
+
+    new_client_missing_data = dict(another_new_client)
+    del new_client_missing_data['client_type_id']
+    create_response = client.post('/client', json=new_client_missing_data, headers=auth_header)
+    assert create_response.status_code == 422
+
+    new_client_wrong_client_type = dict(new_client)
+    new_client_wrong_client_type['client_type_id'] = 100
+    create_response = client.post('/client', json=new_client_wrong_client_type, headers=auth_header)
+    assert create_response.status_code == 400
+    assert create_response.json()['detail'] == 'Client type does not exist'
+
+    delete_response = client.delete(f'/client/{client_id}', headers=auth_header)
     assert delete_response.status_code == 200
 
 def test_update_client(auth_header):
@@ -102,24 +124,6 @@ def test_delete_client(auth_header):
 
     get_response = client.get(f'/clients?client_name={create_response.json()["name"]}', headers=auth_header)
     assert get_response.status_code == 404
-
-def test_create_client_exists(auth_header):
-    response = client.post('/client', json=new_client, headers=auth_header)
-    assert response.status_code == 400
-    assert response.json()['detail'] == 'Client exists'
-
-def test_create_client_missing_data(auth_header):
-    new_client_missing_data = dict(new_client)
-    del new_client_missing_data['client_type_id']
-    response = client.post('/client', json=new_client_missing_data, headers=auth_header)
-    assert response.status_code == 422
-
-def test_create_client_wrong_client_type(auth_header):
-    new_client_wrong_client_type = dict(new_client)
-    new_client_wrong_client_type['client_type_id'] = 100
-    response = client.post('/client', json=new_client_wrong_client_type, headers=auth_header)
-    assert response.status_code == 400
-    assert response.json()['detail'] == 'Client type does not exist'
 
 # test "get_clients"
 def test_get_clients_by_name(auth_header):
