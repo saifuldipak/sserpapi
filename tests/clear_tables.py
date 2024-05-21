bbd6c61e3db599f3feef46d28f0457cd504598fb
Saiful Islam <saiful.kbtel@gmail.com>
2024-05-21 13:01:35 +0600

    Feat: add script clear_tables.py

---
+++ tests/clear_tables.py
@@ -0,0 +1,21 @@
+from sqlalchemy.exc import IntegrityError, DBAPIError
+from sqlalchemy import delete
+import sserpapi.db.models
+from sserpapi.db.connection import SessionLocal
+
+db = SessionLocal()
+
+table_list = ['Services', 'ServiceTypes', 'Pops', 'Vendors', 'Clients', 'ClientTypes']
+
+for table in table_list:
+    stmt = delete(sserpapi.db.models.__dict__[table])
+    try:
+        db.execute(stmt)
+        db.commit()
+    except IntegrityError as e:
+        raise IntegrityError(str(stmt), [], e) from e
+    except Exception as e:
+        raise DBAPIError(str(stmt), [], e) from e
+
+    
+    
