5c07b92deea01b8ec920e2fec1435cd95641094c
Saiful Islam <saiful.kbtel@gmail.com>
2024-05-21 15:30:59 +0600

    Fix: create clear_tables function in clear_tables.py

--- tests/clear_tables.py
+++ tests/clear_tables.py
@@ -3,19 +3,18 @@ from sqlalchemy import delete
 import sserpapi.db.models
 from sserpapi.db.connection import SessionLocal
 
-db = SessionLocal()
+def clear_tables():
+    db = SessionLocal()
 
-table_list = ['Services', 'ServiceTypes', 'Pops', 'Vendors', 'Clients', 'ClientTypes']
+    table_list = ['Services', 'ServiceTypes', 'Pops', 'Vendors', 'Clients', 'ClientTypes']
 
-for table in table_list:
-    stmt = delete(sserpapi.db.models.__dict__[table])
-    try:
-        db.execute(stmt)
-        db.commit()
-    except IntegrityError as e:
-        raise IntegrityError(str(stmt), [], e) from e
-    except Exception as e:
-        raise DBAPIError(str(stmt), [], e) from e
+    for table in table_list:
+        stmt = delete(sserpapi.db.models.__dict__[table])
+        try:
+            db.execute(stmt)
+            db.commit()
+        except IntegrityError as e:
+            raise IntegrityError(str(stmt), [], e) from e
+        except Exception as e:
+            raise DBAPIError(str(stmt), [], e) from e
 
-    
-    
