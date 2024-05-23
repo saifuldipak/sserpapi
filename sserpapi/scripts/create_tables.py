5887ba7239e253a95c3207664b85b9497e40ef98
Saiful Islam <saiful.kbtel@gmail.com>
2024-05-23 15:15:11 +0600

    Fix: move add_user.py and create_tables.py to scripts/

--- sserpapi/scripts/create_tables.py
+++ sserpapi/scripts/create_tables.py
@@ -1,4 +1,5 @@
 from sserpapi.db import connection
 from sserpapi.db import models
 
+connection.Base.metadata.drop_all(bind=connection.engine)
 connection.Base.metadata.create_all(bind=connection.engine)
