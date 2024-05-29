15a194d923d42ad05de381e5a7f16f61d90bb338
Saiful Islam <saiful.kbtel@gmail.com>
2024-05-29 14:20:55 +0600

    Fix: reraise exception using DBAPIError

--- sserpapi/db/dependency.py
+++ sserpapi/db/dependency.py
@@ -1,22 +1,19 @@
 import logging
-from sqlalchemy.exc import OperationalError
+from sqlalchemy.exc import OperationalError, DBAPIError
 from sserpapi.db.connection import SessionLocal
 
 logger = logging.getLogger(__name__)
 
 # Database connection
 def get_db():
-    """
-    Function to get a database connection.
-    Yields a database session.
-    Catches OperationalError and logs the error.
-    Closes the database session in the end.
-    """
     try:
         db = SessionLocal()
         yield db
     except OperationalError as e:
         logger.error('Database connection error: %s', e)
-        raise e
+        raise DBAPIError(str(e), [], e) from e
+    except Exception as e:
+        logger.error('Database connection error: %s', e)
+        raise DBAPIError(str(e), [], e) from e
     finally:
         db.close()
