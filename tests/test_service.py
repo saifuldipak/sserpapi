8a7ca1278b8b279783832ec51f023309ae85d687
Saiful Islam <saiful.kbtel@gmail.com>
2024-06-03 16:35:18 +0600

    Feat: add tests for add_service, get_services and update_service

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,3 +1,12 @@
+#helper functions
+def assert_get_services_response_json(get_services_response_json, service):
+    assert get_services_response_json['point'] == service['point']
+    assert get_services_response_json['client_id'] == service['client_id']
+    assert get_services_response_json['service_type_id'] == service['service_type_id']
+    assert get_services_response_json['bandwidth'] == service['bandwidth']
+    assert get_services_response_json['pop_id'] == service['pop_id']
+    assert get_services_response_json['extra_info'] == service['extra_info']
+
 #test "add_service"
 def test_add_service(add_service, new_service):
     add_service_response = add_service(new_service)
@@ -6,7 +15,6 @@ def test_add_service(add_service, new_service):
     del(add_service_response_json['id'])
     assert add_service_response_json == new_service
 
-#test "add_service_wrong_data"
 def test_add_duplicate_service(add_service, new_service, add_service_only):
     add_service_response = add_service(new_service)
     assert add_service_response.status_code == 200
@@ -14,3 +22,106 @@ def test_add_duplicate_service(add_service, new_service, add_service_only):
     add_service_only_response = add_service_only(new_service)
     assert add_service_only_response.status_code == 400
     assert add_service_only_response.json()['detail'] == 'Service exists'
+
+def test_add_service_wrong_client_id(add_service_only, new_service):
+    new_service['client_id'] = 10001
+    add_service_only_response = add_service_only(new_service)
+    assert add_service_only_response.status_code == 400
+    assert add_service_only_response.json()['detail'] == 'Client not found'
+
+def test_add_service_wrong_service_type_id(add_service_only, new_service, add_client, new_client):
+    add_client_response = add_client(new_client)
+    assert add_client_response.status_code == 200
+
+    new_service['client_id'] = add_client_response.json()['id']
+    new_service['service_type_id'] = 10001
+    add_service_only_response = add_service_only(new_service)
+    assert add_service_only_response.status_code == 400
+    assert add_service_only_response.json()['detail'] == 'Service type not found'
+
+def test_add_service_wrong_pop_id(add_service_only, new_service, add_client, new_client, add_service_type, new_service_type):
+    add_client_response = add_client(new_client)
+    assert add_client_response.status_code == 200
+
+    add_service_type_response = add_service_type(new_service_type)
+    assert add_service_type_response.status_code == 200
+
+    new_service['client_id'] = add_client_response.json()['id']
+    new_service['service_type_id'] = add_service_type_response.json()['id']
+    new_service['pop_id'] = 10001
+    add_service_only_response = add_service_only(new_service)
+    assert add_service_only_response.status_code == 400
+    assert add_service_only_response.json()['detail'] == 'Pop not found'
+
+def test_add_service_blank_body(add_service_only):
+    add_service_only_response = add_service_only({})
+    assert add_service_only_response.status_code == 422
+
+#test "get_services"
+def test_get_services_by_point(add_service, new_service, client, auth_header):
+    add_service_response = add_service(new_service)
+    assert add_service_response.status_code == 200
+
+    get_services_response = client.get(f"/services?service_point={add_service_response.json()['point']}", headers=auth_header)
+    assert get_services_response.status_code == 200
+    assert_get_services_response_json(get_services_response.json()[0], new_service)
+
+def test_get_services_by_client_name(add_service, new_service, client, auth_header):
+    add_service_response = add_service(new_service)
+    assert add_service_response.status_code == 200
+
+    get_clients_response = client.get(f"/clients?client_id={new_service['client_id']}", headers=auth_header)
+    assert get_clients_response.status_code == 200
+
+    get_services_response = client.get(f"/services?client_name={get_clients_response.json()[0]['name']}", headers=auth_header)
+    assert get_services_response.status_code == 200
+    assert_get_services_response_json(get_services_response.json()[0], new_service)
+
+def test_get_services_by_pop_name(add_service, new_service, client, auth_header):
+    add_service_response = add_service(new_service)
+    assert add_service_response.status_code == 200
+
+    get_pops_response = client.get(f"/pops?pop_id={new_service['pop_id']}", headers=auth_header)
+    assert get_pops_response.status_code == 200
+
+    get_services_response = client.get(f"/services?pop_name={get_pops_response.json()[0]['name']}", headers=auth_header)
+    assert_get_services_response_json(get_services_response.json()[0], new_service)
+
+def test_get_services_by_point_client_pop(add_service, new_service, client, auth_header):
+    add_service_response = add_service(new_service)
+    assert add_service_response.status_code == 200
+
+    get_clients_response = client.get(f"/clients?client_id={new_service['client_id']}", headers=auth_header)
+    assert get_clients_response.status_code == 200
+
+    get_pops_response = client.get(f"/pops?pop_id={new_service['pop_id']}", headers=auth_header)
+    assert get_pops_response.status_code == 200
+
+    get_services_response = client.get(f"/services?service_point={new_service['point']}&client_name={get_clients_response.json()[0]['name']}&pop_name={get_pops_response.json()[0]['name']}", headers=auth_header)
+    assert_get_services_response_json(get_services_response.json()[0], new_service)
+
+def test_get_services_by_wrong_parameters(client, auth_header):
+    get_services_response = client.get("/services?service_point=wrong_point", headers=auth_header)
+    assert get_services_response.status_code == 404
+    assert get_services_response.json()['detail'] == 'No service found'
+
+    get_services_response = client.get("/services?client_name=wrong_client_name", headers=auth_header)
+    assert get_services_response.status_code == 404
+    assert get_services_response.json()['detail'] == 'No service found'
+
+    get_services_response = client.get("/services?pop_name=wrong_pop_name", headers=auth_header)
+    assert get_services_response.status_code == 404
+    assert get_services_response.json()['detail'] == 'No service found'
+
+
+#test "update_service"
+def test_update_service(add_service, new_service, new_service_updated, client, auth_header):
+    add_service_response = add_service(new_service)
+    assert add_service_response.status_code == 200
+
+    new_service_updated['id'] = add_service_response.json()['id']
+    new_service_updated['client_id'] = add_service_response.json()['client_id']
+    new_service_updated['service_type_id'] = add_service_response.json()['service_type_id']
+    new_service_updated['pop_id'] = add_service_response.json()['pop_id']
+    update_service_response = client.put('/service', json=new_service_updated, headers=auth_header)
+    assert_get_services_response_json(update_service_response.json(), new_service_updated)
