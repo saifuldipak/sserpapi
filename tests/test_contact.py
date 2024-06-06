b9d306c8d016063f0becc17f878765ff6febe7a4
Saiful Islam <saiful.kbtel@gmail.com>
2024-06-06 17:52:13 +0600

    Feat: add tests for add_contact and get_contacts

--- tests/test_contact.py
+++ tests/test_contact.py
@@ -1,5 +1,5 @@
 #helper function
-def assert_contact_response_json(contact_response_json, contact):
+def assert_contact_response_json(contact_response_json, contact, name = None):
     assert contact_response_json['name'] == contact['name']
     assert contact_response_json['designation'] == contact['designation']
     assert contact_response_json['type'] == contact['type']
@@ -12,10 +12,16 @@ def assert_contact_response_json(contact_response_json, contact):
         assert contact_response_json['email'] == contact['email']
     if contact_response_json['client_id'] is not None:
         assert contact_response_json['client_id'] == contact['client_id']
+        if name:
+            assert contact_response_json['clients']['name'] == name
     if contact_response_json['vendor_id'] is not None:
         assert contact_response_json['vendor_id'] == contact['vendor_id']
+        if name:
+            assert contact_response_json['vendors']['name'] == name
     if contact_response_json['service_id'] is not None:
         assert contact_response_json['service_id'] == contact['service_id']
+        if name:
+            assert contact_response_json['services']['point'] == name
 
 #test "add_contact"
 def test_add_contact(add_service, new_service, add_contact, new_contact):
@@ -45,3 +51,199 @@ def test_add_contact_by_wrong_data(add_contact, new_contact):
     assert add_contact_response.status_code == 400
     assert add_contact_response.json()['detail'] == 'Service not found'
 
+def test_add_contact_missing_body(add_contact):
+    add_contact_response = add_contact({})
+    assert add_contact_response.status_code == 422
+
+#test "get_contacts"
+def test_get_contacts_by_id(add_service, new_service, add_contact, new_contact, client, auth_header):
+    add_service_response = add_service(new_service)
+    assert add_service_response.status_code == 200
+    
+    new_contact['service_id'] = add_service_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?contact_id={add_contact_response.json()['id']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_service_response.json()['point'])
+
+def test_get_contacts_by_name(add_vendor, new_vendor, add_contact, new_contact, client, auth_header):
+    add_vendor_response = add_vendor(new_vendor)
+    assert add_vendor_response.status_code == 200
+    
+    new_contact['vendor_id'] = add_vendor_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?contact_name={new_contact['name']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_vendor_response.json()['name'])
+
+def test_get_contacts_by_designation(add_service, new_service, add_contact, new_contact, client, auth_header):
+    add_service_response = add_service(new_service)
+    assert add_service_response.status_code == 200
+    
+    new_contact['service_id'] = add_service_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?designation={new_contact['designation']}&service_id={new_contact['service_id']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_service_response.json()['point'])
+
+def test_get_contacts_by_type(add_client, new_client, add_contact, new_contact, client, auth_header):
+    add_client_response = add_client(new_client)
+    assert add_client_response.status_code == 200
+    
+    new_contact['client_id'] = add_client_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?type={new_contact['type']}&client_id={new_contact['client_id']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_client_response.json()['name'])
+
+def test_get_contacts_by_phone1(add_client, new_client, add_contact, new_contact, client, auth_header):
+    add_client_response = add_client(new_client)
+    assert add_client_response.status_code == 200
+    
+    new_contact['client_id'] = add_client_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?phone1={new_contact['phone1']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_client_response.json()['name'])
+
+def test_get_contacts_by_email(add_client, new_client, add_contact, new_contact, client, auth_header):
+    add_client_response = add_client(new_client)
+    assert add_client_response.status_code == 200
+    
+    new_contact['client_id'] = add_client_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?email={new_contact['email']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_client_response.json()['name'])
+
+def test_get_contacts_by_client_id(add_client, new_client, add_contact, new_contact, client, auth_header):
+    add_client_response = add_client(new_client)
+    assert add_client_response.status_code == 200
+    
+    new_contact['client_id'] = add_client_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?client_id={new_contact['client_id']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_client_response.json()['name'])
+
+def test_get_contacts_by_service_id(add_service, new_service, add_contact, new_contact, client, auth_header):
+    add_service_response = add_service(new_service)
+    assert add_service_response.status_code == 200
+    
+    new_contact['service_id'] = add_service_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?service_id={new_contact['service_id']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_service_response.json()['point'])
+
+def test_get_contacts_by_vendor_id(add_vendor, new_vendor, add_contact, new_contact, client, auth_header):
+    add_vendor_response = add_vendor(new_vendor)
+    assert add_vendor_response.status_code == 200
+    
+    new_contact['vendor_id'] = add_vendor_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?vendor_id={new_contact['vendor_id']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_vendor_response.json()['name'])
+
+def test_get_contacts_by_client_name(add_client, new_client, add_contact, new_contact, client, auth_header):
+    add_client_response = add_client(new_client)
+    assert add_client_response.status_code == 200
+    
+    new_contact['client_id'] = add_client_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?client_name={add_client_response.json()['name']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_client_response.json()['name'])
+
+def test_get_contacts_by_vendor_name(add_vendor, new_vendor, add_contact, new_contact, client, auth_header):
+    add_vendor_response = add_vendor(new_vendor)
+    assert add_vendor_response.status_code == 200
+    
+    new_contact['vendor_id'] = add_vendor_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?vendor_name={add_vendor_response.json()['name']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_vendor_response.json()['name'])
+
+def test_get_contacts_by_service_point(add_service, new_service, add_contact, new_contact, client, auth_header):
+    add_service_response = add_service(new_service)
+    assert add_service_response.status_code == 200
+    
+    new_contact['service_id'] = add_service_response.json()['id']
+    add_contact_response = add_contact(new_contact)
+    assert add_contact_response.status_code == 200
+
+    get_clients_response = client.get(f"/clients?client_id={add_service_response.json()['client_id']}", headers=auth_header)
+    assert get_clients_response.status_code == 200
+
+    get_contacts_response = client.get(f"/contacts?service_point={add_service_response.json()['point']}&client_name={get_clients_response.json()[0]['name']}", headers=auth_header)
+    assert get_contacts_response.status_code == 200
+    assert_contact_response_json(get_contacts_response.json()[0], new_contact, add_service_response.json()['point'])
+
+def test_get_contacts_missing_parameters(client, auth_header):
+    get_contacts_response = client.get("/contacts", headers=auth_header)
+    assert get_contacts_response.status_code == 400
+    assert get_contacts_response.json()['detail'] == 'No query parameters'
+
+def test_get_contacts_by_designation_or_type_missing_parameters(client, auth_header):
+    get_contacts_response = client.get("/contacts?designation=some_designation", headers=auth_header)
+    assert get_contacts_response.status_code == 400
+    assert get_contacts_response.json()['detail'] == 'Must give at least one query parameter(client_id, service_id, vendor_id, client_name, service_point, vendor_name) when designation or contact_type is provided'
+
+def test_get_contacts_by_client_service_vendor_ids(client, auth_header):
+    get_contacts_response = client.get("/contacts?client_id=1&service_id=1&vendor_id=1", headers=auth_header)
+    assert get_contacts_response.status_code == 400
+    assert get_contacts_response.json()['detail'] == 'client_id, service_id, and vendor_id cannot be provided at the same time'
+
+def test_get_contacts_by_client_name_and_vendor_name(client, auth_header):
+    get_contacts_response = client.get("/contacts?client_name=client_name&vendor_name=vendor_name", headers=auth_header)
+    assert get_contacts_response.status_code == 400
+    assert get_contacts_response.json()['detail'] == 'client_name and vendor_name cannot be provided at the same time'
+
+def test_get_contacts_by_service_id_and_service_point(client, auth_header):
+    get_contacts_response = client.get("/contacts?service_id=1100&service_point=service_point", headers=auth_header)
+    assert get_contacts_response.status_code == 400
+    assert get_contacts_response.json()['detail'] == 'service_id and service_point both cannot be provided at the same time'
+
+def test_get_contacts_by_client_id_and_client_name(client, auth_header):
+    get_contacts_response = client.get("/contacts?client_id=1100&client_name=client_name", headers=auth_header)
+    assert get_contacts_response.status_code == 400
+    assert get_contacts_response.json()['detail'] == 'client_id and client_name both cannot be provided at the same time'
+
+def test_get_contacts_by_vendor_id_and_vendor_name(client, auth_header):
+    get_contacts_response = client.get("/contacts?vendor_id=1100&vendor_name=vendor_name", headers=auth_header)
+    assert get_contacts_response.status_code == 400
+    assert get_contacts_response.json()['detail'] == 'vendor_id and vendor_name both cannot be provided at the same time'
+
+def test_get_contacts_by_designation_and_contact_type_missing_ids(client, auth_header):
+    get_contacts_response = client.get("/contacts?designation=designation&contact_type=Technical", headers=auth_header)
+    assert get_contacts_response.status_code == 400
+    assert get_contacts_response.json()['detail'] == 'Must give at least one query parameter(client_id, service_id, vendor_id, client_name, service_point, vendor_name) when designation or contact_type is provided'
+
+def test_get_contacts_by_service_point_missing_client_name(client, auth_header):
+    get_contacts_response = client.get("/contacts?service_point=service_point", headers=auth_header)
+    assert get_contacts_response.status_code == 400
+    assert get_contacts_response.json()['detail'] == 'Must give query parameter client_name when service_name is provided'
