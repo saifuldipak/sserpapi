00a6f7588127dd16df767c906d11c161d40b75eb
Saiful Islam <saiful.kbtel@gmail.com>
2024-06-12 16:44:06 +0600

    Fix: rename create_user() to add_user() and add exception handling

--- sserpapi/routers/users.py
+++ sserpapi/routers/users.py
@@ -1,5 +1,5 @@
 import logging
-from fastapi import APIRouter, Depends, HTTPException, Security
+from fastapi import APIRouter, Depends, HTTPException, Security, status
 from sqlalchemy.orm import Session
 import sserpapi.db.queries as db_query
 from sserpapi.db.dependency import get_db
@@ -8,14 +8,19 @@ import sserpapi.pydantic_schemas as schemas
 
 logger = logging.getLogger(__name__)
 
-router = APIRouter(dependencies=[Security(get_current_active_user, scopes=["Admin", "Editor"])])
+router = APIRouter(dependencies=[Security(get_current_active_user, scopes=["admin"])])
 
-@router.post("/users/add", response_model=schemas.UserBase, summary='Add an user', tags=['Users'])
-def create_user(user: schemas.User, db: Session = Depends(get_db)):
-    user_exists = db_query.get_user_by_name(db, user_name=user.user_name)
+@router.post("/user", response_model=schemas.UserBase, summary='Add an user', tags=['Users'])
+def add_user(user: schemas.User, db: Session = Depends(get_db)):
+    try:
+        user_exists = db_query.get_user_by_name(db, user_name=user.user_name)
+    except Exception as e:
+        logger.error('get_user_by_name(): %s', e)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) from e
+    
     if user_exists:
-        logger.warning('User "%s" exists', user.user_name)
-        raise HTTPException(status_code=400, detail="User exists")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User exists")
+    
     user.password = get_password_hash(user.password)
     return db_query.add_user(db=db, user=user)
 
