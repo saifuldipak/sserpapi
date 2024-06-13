f553cb5ac7eafea3ff948f8e2a30fadf51c78555
Saiful Islam <saiful.kbtel@gmail.com>
2024-06-13 13:03:00 +0600

    Fix: rename read_clients() to get_users(), add query parameters, add exception handling

--- sserpapi/routers/users.py
+++ sserpapi/routers/users.py
@@ -25,8 +25,14 @@ def add_user(user: schemas.User, db: Session = Depends(get_db)):
     return db_query.add_user(db=db, user=user)
 
 
-@router.get("/users/show", response_model=list[schemas.UserBase], summary='Get all users', tags=['Users'])
-def read_clients(page: int = 0, page_size: int = 10, db: Session = Depends(get_db)):
-    offset = (page - 1) * page_size
-    db_clients = db_query.get_users(db, offset=offset, limit=page_size)
+@router.get("/users", response_model=list[schemas.UserBase], summary='Get all users', tags=['Users'])
+def get_users(user_name: str | None = None, disabled: bool | None = None, scope: str | None = None, page: int = 0, page_size: int = 10, db: Session = Depends(get_db)):
+    offset = page * page_size
+
+    try:
+        db_clients = db_query.get_users(db, user_name=user_name, disabled=disabled, scope=scope, offset=offset, limit=page_size)
+    except Exception as e:
+        logger.error('get_users(): %s', e)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) from e
+    
     return db_clients
