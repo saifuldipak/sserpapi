37e66c898620b313f13ae4aa1b301c3be0a1743f
Saiful Islam <saiful.kbtel@gmail.com>
2024-07-02 10:58:26 +0600

    Fix: modify variable name config_file to logging_config_file

--- sserpapi/main.py
+++ sserpapi/main.py
@@ -10,8 +10,8 @@ import os
 # Configure logging
 load_dotenv()
 FASTAPI_PATH=os.getenv('FASTAPI_PATH')
-config_file = os.path.join(FASTAPI_PATH, 'logging.yaml')
-with open(config_file, 'r', encoding='utf-8') as f:
+logging_config_file = os.path.join(FASTAPI_PATH, 'logging.yaml')
+with open(logging_config_file, 'r', encoding='utf-8') as f:
     config_dict = yaml.safe_load(f)
     config_dict['handlers']['file']['filename'] = os.path.join(FASTAPI_PATH, config_dict['handlers']['file']['filename'])
 
