137a28eee55f74ff54b4db90973947779ef04f31
Saiful Islam <saiful.kbtel@gmail.com>
2024-07-11 14:31:17 +0600

    Fix: add docstring and specific exception 'OperationalError'

--- sserpapi/db/dependency.py
+++ sserpapi/db/dependency.py
@@ -1,14 +1,25 @@
 import logging
+from sqlalchemy.exc import OperationalError
 from sserpapi.db.connection import SessionLocal
 
 logger = logging.getLogger(__name__)
 
 # Database connection
 def get_db():
+    """
+    Creates a database session and yields it.
+
+    Returns:
+        A database session.
+
+    Raises:
+        OperationalError: If there is an error connecting to the database.
+
+    """
     try:
         db = SessionLocal()
         yield db
-    except Exception as e:
+    except OperationalError as e:
         logger.error("get_db(): Database connection error: %s", e)
         raise e
     finally:
