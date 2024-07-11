684f79dbd2f16357ff055fc2bc828ebe49f7cd0c
Saiful Islam <saiful.kbtel@gmail.com>
2024-07-11 14:32:43 +0600

    Fix: add more exception handling

--- sserpapi/auth.py
+++ sserpapi/auth.py
@@ -21,7 +21,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 500
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token', scopes={"Admin": "Admin user with full acces", "User": "General user with limited access", "editor": "Add or modify users"})
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token', scopes={"admin": "Can read & write to any table", "write": "Can read & write to any table except users", "read": "Can read any table except users"})
 
 router = APIRouter()
 
@@ -35,8 +35,11 @@ def verify_password(plain_password, hashed_password):
 def authenticate_user(db: Session, username: str, password: str):
     user = db_query.get_user_by_name(db, username)
     if not user:
+        logger.warning('User "%s" not found', username)
         return False
+    
     if not verify_password(password, user.password):
+        logger.warning('User "%s" password incorrect', username)
         return False
     
     return user
@@ -72,9 +75,9 @@ def get_current_user(security_scopes: SecurityScopes, token: Annotated[str, Depe
         token_scopes = payload.get("scopes", [])
         token_data = schemas.TokenData(scopes=[token_scopes], username=username)
     except JWTError as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f'{e}')
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f'{e}') from e
     except ValidationError as e:
-        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=f'{e}')
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=f'{e}') from e
     
     user = db_query.get_user_by_name(db=db, user_name=token_data.username)
     if user is None:
@@ -103,16 +106,15 @@ async def get_current_active_user(current_user: Annotated[schemas.User, Security
 
 
 @router.post("/token", response_model=schemas.Token)
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
-    user = authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        logger.warning('User authentication failed for "%s"', form_data.username)
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+async def get_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+    if not db:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database connection error")
+    
+    user_authenticated = authenticate_user(db, form_data.username, form_data.password)
+    if not user_authenticated:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+    
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.user_name, "scopes": user.scope},expires_delta=access_token_expires,)
+    access_token = create_access_token(data={"sub": user_authenticated.user_name, "scopes": user_authenticated.scope},expires_delta=access_token_expires,)
     
     return {"access_token": access_token, "token_type": "bearer"}
