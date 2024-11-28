5fe36c23a1055da2569975f4616cc1ee3aca4a4c
Saiful Islam Dipak <saiful.kbtel@gmail.com>
2024-11-28 15:54:26 +0600

    add test_search_clients_by_client_type in test_search.py

--- tests/test_search.py
+++ tests/test_search.py
@@ -14,17 +14,24 @@ def assert_search_response(search_response, new_clients, new_client_type, total,
         assert search_response.json().get('results')[i].get('name') == new_clients_sorted_subset[i].get('name')
         assert search_response.json().get('results')[i]['client_types']['name'] == new_client_type.json().get('name')
 
-
-def test_search_clients(auth_header, client, add_clients, new_client):
+def test_search_clients(auth_header, client, add_clients, new_client, new_client_type):
     clients = 100
-    (new_clients, new_client_type, another_new_client_type)  = add_clients(clients)
+    (new_clients, new_client_type)  = add_clients(clients, new_client_type)
     get_clients_response = client.get(f"/search?query={new_client['name']}&resource_type=clients", headers=auth_header)
     assert_search_response(get_clients_response, new_clients, new_client_type, clients, 1, 20)   
 
-def test_search_clients_with_page_and_items_per_page(auth_header, client, add_clients, new_client):
+def test_search_clients_with_page_and_items_per_page(auth_header, client, add_clients, new_client, new_client_type):
     clients = 100
     page = 5
     items_per_page = 10
-    (new_clients, new_client_type, another_new_client_type)  = add_clients(clients)
+    (new_clients, new_client_type)  = add_clients(clients, new_client_type)
     get_clients_response = client.get(f"/search?query={new_client['name']}&resource_type=clients&page={page}&items_per_page={items_per_page}", headers=auth_header)
+    assert_search_response(get_clients_response, new_clients, new_client_type, clients, page, items_per_page)
+
+def test_search_clients_by_client_type(auth_header, client, add_clients, new_client, new_client_type):
+    clients = 50
+    page = 2
+    items_per_page = 10
+    (new_clients, new_client_type) = add_clients(clients, new_client_type)
+    get_clients_response = client.get(f"/search?query={new_client['name']}&resource_type=clients&filter_by={new_client_type.json()['name']}&page={page}&items_per_page={items_per_page}", headers=auth_header)
     assert_search_response(get_clients_response, new_clients, new_client_type, clients, page, items_per_page)
