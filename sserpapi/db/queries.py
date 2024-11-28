7b3462e6982bc94c76e20c610a8fd403452ef50d
Saiful Islam Dipak <saiful.kbtel@gmail.com>
2024-11-28 15:08:28 +0600

    optimize query and load data from joined tables in db.queries.get_clients()

--- sserpapi/db/queries.py
+++ sserpapi/db/queries.py
@@ -1,6 +1,6 @@
 # pylint: disable=E0401
 import logging
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from sqlalchemy.exc import IntegrityError, DBAPIError, SQLAlchemyError
 from sqlalchemy import delete
 from sserpapi.db import models
@@ -58,27 +58,21 @@ def get_clients(db: Session, client_name: str | None = None, client_type: str | 
     if not client_name and not client_type and not client_id:
         raise TypeError('Must provide at least client_name, client_type or client_id')
     
-    base_query = db.query(models.Clients)
-
-    if (client_name):
-        client_name_string = f'{client_name}%'
-
-    if (client_type):
-        client_type_string = f'{client_type}%'
+    base_query = db.query(models.Clients).options(joinedload(models.Clients.client_types))
 
     if client_name:
-        base_query =  base_query.filter(models.Clients.name.ilike(client_name_string))
+        base_query =  base_query.filter(models.Clients.name.ilike(f"{client_name}%"))
 
     if client_type:
-        base_query = base_query.join(models.ClientTypes).filter(models.ClientTypes.name.ilike(client_type_string))
+        base_query = base_query.join(models.ClientTypes).filter(models.ClientTypes.name.ilike(f"{client_type}%"))
 
     if client_id:
         base_query = base_query.filter(models.Clients.id==client_id)
 
     base_query = base_query.order_by(models.Clients.name)
     try:
-        total_count = base_query.count()
-        clients = base_query.offset(offset).limit(limit).all()
+        total_count = base_query.order_by(None).count()
+        clients = base_query.order_by(models.Clients.name).offset(offset).limit(limit).all()
         return clients, total_count
     except Exception as e:
         raise e
